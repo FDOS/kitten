54227437042ad752c9bafa08fd41c573b3557f25
Andrew Bird <ajb@spheresystems.co.uk>
2022-01-24 18:41:15 +0000

    Test: Add test for kitten_extract_response()

--- test/nls/test.es
+++ test/nls/test.es
@@ -6,3 +6,4 @@
 1.1:Quiero leer el mensaje
 1.2:Usted puede traer un mensaje para ella
 2.0:Ella no tiene ese mensaje
+3.0:De verdad borrar este mensaje (S/N)? 
--- test/test.c
+++ test/test.c
@@ -10,11 +10,17 @@
 #define MSG3 "Look in the second class for the second message"
 #define RSP3 "Quiero leer el mensaje"
 
+// Please excuse the lack of an opening question mark on Spanish translation
+#define MSG4 "Really delete this message (Y/N)? "
+#define RSP4 "De verdad borrar este mensaje (S/N)? "
+#define KEY4 "SN"
+
 
 int main(void) {
 
   nl_catd cat;
-  char *m;
+  const char *m;
+  char tmpstr[10];
 
   putenv("NLSPATH=test\\nls");
   putenv("LANG=es");
@@ -40,6 +46,33 @@ int main(void) {
   }
   if (strcmp(RSP3, m) != 0) {
     puts("FAIL: MSG3 was not replaced with correct Spanish");
+    return 1;
+  }
+
+  m = catgets(cat, 3, 0, MSG4);
+  if (strcmp(MSG4, m) == 0) {
+    puts("FAIL: MSG4 was not replaced");
+    return 1;
+  }
+  if (strcmp(RSP4, m) != 0) {
+    puts("FAIL: MSG4 was not replaced with correct Spanish");
+    return 1;
+  }
+  if (!kitten_extract_response(m, "(/)", tmpstr, 2)) {
+    puts("FAIL: RSP4 was not parsed");
+    return 1;
+  }
+  if (tmpstr[0] != 'S' || tmpstr[1] != 'N') {
+    puts("FAIL: RSP4 was not parsed correctly");
+    return 1;
+  }
+
+  if (!kitten_extract_response("Choose [A:B:C:D]", "[:]", tmpstr, 4)) {
+    puts("FAIL: \"Choose [A:B:C:D]\" was not parsed");
+    return 1;
+  }
+  if (tmpstr[0] != 'A' || tmpstr[1] != 'B' || tmpstr[2] != 'C' || tmpstr[3] != 'D') {
+    puts("FAIL: \"Choose [A:B:C:D]\" was not parsed correctly");
     return 1;
   }
 
